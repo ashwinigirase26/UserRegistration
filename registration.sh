0b2bd95fc2fd51d60ebec88c04ac7adde6d48d3a
“Enter <ashwinigirase26@gmail.com>
2020-07-09 12:05:07 +0530

    email id

--- registration.sh
+++ registration.sh
@@ -23,3 +23,13 @@ else
         echo "Last name is invalid";
 fi
 
+# validate  users email id
+echo "Enter your email id"
+read Email;
+pattern1="^[0-9a-zA-Z]+([+_-.][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z,]{2,4}([.][0-9a-zA-Z,]{2})";
+if [[ $Email =~ $pattern1 ]]
+then
+	echo "entered email is valid";
+else
+	echo "entered email ids invalid";
+fi
